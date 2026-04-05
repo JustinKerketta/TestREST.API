912b2b1d8249bddff77c6b4c6c31775c2da4ec82
Jus Kett <juskett@hotmail.com>
2026-04-05 00:43:51 -0400

    Move REST endpoints to a seperate extension class

--- TestREST.API/Program.cs
+++ TestREST.API/Program.cs
@@ -1,5 +1,4 @@
-using AutoMapper;
-using TestREST.API.Dtos;
+using TestREST.API.Endpoints;
 
 namespace TestREST.API;
 
@@ -11,71 +10,10 @@ public class Program
     builder.Services.AddAutoMapper(cfg => {
       cfg.AddMaps(typeof(Program));
     });
+
     var app = builder.Build();
 
-    List<GroupDto> groupDtos = [
-      new GroupDto(1, "Adminstrator 01",  GroupTypeDto.Security,      "Administrator of Group"),
-      new GroupDto(2, "Adminstrator 02",  GroupTypeDto.Security,      "Administrator of Group"),
-      new GroupDto(3, "Glenn Close",      GroupTypeDto.Microsoft365,  "Regular User"),
-      new GroupDto(4, "Michael Douglas",  GroupTypeDto.Microsoft365,  "Regular User"),
-      new GroupDto(5, "Anne Archer",      GroupTypeDto.Security,      "Regular User"),
-      new GroupDto(6, "Jane Krawkowski",  GroupTypeDto.Security,      "Regular User"),
-    ];
-
-    app.MapGet("/", () => "Hello World!");
-
-    //GET /groups
-    app.MapGet("/groups", () => 
-    {
-      return Results.Ok(groupDtos);
-    });
-
-    //GET /groups/1
-    app.MapGet("/groups/{id}", (int id) => 
-    {
-      GroupDto? groupDto = groupDtos.Find(group => group.Id == id);
-      if (groupDto == null)
-      {
-        return Results.NotFound();
-      }
-      return Results.Ok(groupDto);
-    }).WithName("GetGroup");
-
-    // POST /groups/
-    app.MapPost("/groups", (CreateGroupDto newCreateGroupDto, IMapper mapper) => {
-      int newId = groupDtos.Count+1;
-      GroupDto newGroupDto = mapper.Map<GroupDto>(
-        newCreateGroupDto, opt => opt.Items["NewId"] = newId);
-      groupDtos.Add(newGroupDto);
-      return Results.CreatedAtRoute("GetGroup", new { id = newGroupDto.Id }, newGroupDto);
-    });
-
-    // PUT /groups/id
-    app.MapPut("/groups/{id}", (int id, UpdateGroupDto updateGroupDto, IMapper mapper) =>
-    {
-      GroupDto? groupDto = groupDtos.Find(group => group.Id == id);
-      if (groupDto != null)
-      {
-        GroupDto updatedGroupDto = mapper.Map<GroupDto>(
-          updateGroupDto, opt => opt.Items["NewId"] = groupDto.Id);
-        groupDtos.Remove(groupDto);
-        groupDtos.Add(updatedGroupDto);
-        return Results.NoContent();
-      }
-      return Results.NotFound();
-    });
-
-    // Delete /groups/id
-    app.MapDelete("/groups/{id}", (int id) =>
-    {
-      GroupDto? groupDto = groupDtos.Find(group => group.Id == id);
-      if (groupDto != null)
-      {
-        groupDtos.Remove(groupDto);
-        return Results.NoContent();
-      }
-      return Results.NotFound();
-    });
+    app.MapRESTEndpoints();
 
     app.Run();
   }
