898719c9dae5c225ca661de5f15fb927a019fef9
Jus Kett <juskett@hotmail.com>
2026-04-08 19:53:57 -0400

    Remove the in-memory list of groups

--- TestREST.API/Endpoints/GroupEndpoints.cs
+++ TestREST.API/Endpoints/GroupEndpoints.cs
@@ -8,15 +8,6 @@ namespace TestREST.API.Endpoints;
 
 public static class GroupEndpoints
 {
-  private static readonly List<GroupDto> groupDtos = [
-      new GroupDto(1, "Adminstrator 01",        GroupTypeDto.Security,      "Administrator of Group"),
-      new GroupDto(2, "Adminstrator 02",        GroupTypeDto.Security,      "Administrator of Group"),
-      new GroupDto(3, "Glenn Close group",      GroupTypeDto.Microsoft365,  "Regular User"),
-      new GroupDto(4, "Michael Douglas group",  GroupTypeDto.Microsoft365,  "Regular User"),
-      new GroupDto(5, "Anne Archer group",      GroupTypeDto.Security,      "Regular User"),
-      new GroupDto(6, "Jane Krawkowski group",  GroupTypeDto.Security,      "Regular User"),
-    ];
-
   public static void MapRESTEndpoints(this WebApplication app)
   {
     app.MapGet("/", () => "Welcome to groups and users!");
